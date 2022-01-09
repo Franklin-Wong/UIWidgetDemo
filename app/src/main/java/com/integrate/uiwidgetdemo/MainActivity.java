bb6367e77ec70bf85f43cf112ef97a1d8aef32b7
wang <developer_senior@163.com>
2022-01-10 01:14:18 +0800

    Initial commit

---
+++ app/src/main/java/com/integrate/uiwidgetdemo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.integrate.uiwidgetdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ShortcutBottomSheet bottomSheet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView showBottom = findViewById(R.id.show);
+        showBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showBottomSheet();
+            }
+        });
+    }
+
+    private void showBottomSheet() {
+        bottomSheet = new ShortcutBottomSheet();
+        bottomSheet.show(getSupportFragmentManager(), "bottomSheet");
+
+    }
+
+    public void hideBottomSheet(View view) {
+        if (bottomSheet != null) {
+            bottomSheet.dismiss();
+        }
+    }
+}
---
+++ app/src/main/java/com/integrate/uiwidgetdemo/ShortcutBottomSheet.java
@@ -0,0 +1,90 @@
+package com.integrate.uiwidgetdemo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ShortcutBottomSheet extends BottomSheetDialogFragment {
+
+    @Override
+    public View onCreateView(@NonNull  LayoutInflater inflater, @Nullable  ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bottomsheet_layout, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.recycleView);
+
+        handleList(view);
+        return view;
+    }
+
+    private void handleList(View view) {
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycleView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(Objects.requireNonNull(getActivity()),DividerItemDecoration.VERTICAL));
+        MyRecyclerAdapter adapter = new MyRecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+
+        adapter.setData(getDatas());
+        adapter.notifyDataSetChanged();
+    }
+
+    private List<String> getDatas() {
+        List<String> list = new ArrayList<>();
+        for (int i = 0 ;i<30 ;i++) {
+            list.add("android："+i);
+        }
+        return list;
+    }
+
+    public static class MyRecyclerAdapter extends RecyclerView.Adapter{
+
+        private List<String> mData ;
+
+        public void setData(List<String> list) {
+            mData = list ;
+        }
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.bottom_sheet_item,null));
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            MyViewHolder myViewHolder = (MyViewHolder) holder;
+            myViewHolder.index.setText(mData.get(position)+"");
+//            myViewHolder.name.setText(mData.get(position)+"");
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData == null ? 0 :mData.size();
+        }
+
+
+
+        public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+            private TextView name ;
+            private TextView index ;
+            public MyViewHolder(View itemView) {
+                super(itemView);
+                name = (TextView) itemView.findViewById(R.id.bottom_sheet_dialog_item_name);
+//                index = (TextView) itemView.findViewById(R.id.bottom_sheet_dialog_item_index);
+            }
+        }
+    }
+}
