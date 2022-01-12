5ec00b5f93e1068117b341ee23353f3590ddf2a9
wangerfeng <wangerfeng@zuler.io>
2022-01-12 20:49:51 +0800

    testString

--- app/src/main/java/com/integrate/uiwidgetdemo/MainActivity.java
+++ app/src/main/java/com/integrate/uiwidgetdemo/MainActivity.java
@@ -22,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Todo
                 showBottomSheet();
-                stringTest = "dev_abc";
+
+                stringTest = "Dev1111";
+
             }
         });
     }
